f0ff8b02932c16195a435d4fb54cb6e6d2f85c92
Kevin Trotter <cskevint@gmail.com>
2013-09-29 14:30:58 -0700

    separated more page content

--- index.php
+++ index.php
@@ -161,30 +161,7 @@
     <h1>Donations</h1>
     <div class="row">
         <div class="col-lg-8">
-            <p>
-                100% of all contributions go directly to fund the programs we support.&nbsp; Foundation staff and business expenses are contributed voluntarily or by the for-profit sponsoring company. Meridian is a 501c3 not for profit charity, EIN # 68-0444965.&nbsp;&nbsp;
-            </p>
-            <p>
-                To make a donation by check, please send to:
-            </p>
-            <p>
-                Donate by Credit Card:
-            </p>
-            <p>
-                Click below&nbsp; $5.00 minimum
-            </p>
-            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-                <input type="image" alt="Make payments with PayPal - it's fast, free and secure!" name="I1" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif">
-                <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHqQYJKoZIhvcNAQcEoIIHmjCCB5YCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBxP4pUfs3q8il0Ec+Fs9SFtCJTQBp0R8ANqhwczvPoJ+RVnsoKr0+pln0fbznrMIsni3pQaorT/J6cgj2sDiq9YHjyhynspzBzxxVZ2lm6EPTBAxSZ5IAw6hLrR4b22DdKXJebDq0utDQHbVwuMvsxezEYJhViG4eiWmOqGj987TELMAkGBSsOAwIaBQAwggElBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECGRuhQDAwAJcgIIBAHg9BvBmt8ELYLeu6gqsMPb3+NQOVftxZ9jQFDCyQzVoPQxiwDvcW0335bLYUVJukD1stcNTX/49SZewdCBAsvF7XPtrcNNZNm/ceouqnNQdoGQfdrgBrnMHY2+z0z9bcDQyyxMB8y7NV2M3Qs6htzx3wn7Hh1u/iYo/0rKma60gCB6Wc5i4AapjiFiauScmGbKs91EsK8Rutg6BQZexAxBfm8430ph+NGqLap75azgF6lcklgigZ8keMnUnErX+j4VgZ5UV3mKdGarJ7+CRypaujhnDwkZ3s8Jo253xkSi9/PJLZbmYUtmCjRJjoQSupRx4v0byCjrTSSFe03+4zkegggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0wNjAyMDYyMjE0MTNaMCMGCSqGSIb3DQEJBDEWBBQgb71m9PYd1aL2vmXyHVyDg6CQ4TANBgkqhkiG9w0BAQEFAASBgGW3VfbDG7eaT5EHq/Jg683zAADjGRF97BvJoICPrq5JeSfrtFyua3Hv6elpicfb7QF7S1LBHxIB/OmckHUVZCMmJ/+CEZuuTpyypyPJ3l6aQTxYFcvH3VwSX3IOLnPP6tsr46Aohqpyq+CNT8W/1YAKf0CqgL89nv0GLWLdcBRj-----END PKCS7----- ">
-                <br>
-                <input type="hidden" name="cmd" value="_s-xclick">
-            </form>
-            <p>
-                <strong>
-                    Gift Option:
-                </strong>
-                To make this donation a holiday, birthday, memorial or other gift, please email us the name, address, occasion and your message.&nbsp; A card will be sent to the recipient to notify them that a charitable contribution has been made in their honor. Please email a description to the address below.
-            </p>
+           <?php include 'pages/donations.php'; ?>
         </div>
         <div class="col-lg-4">
             <blockquote>
@@ -216,7 +193,7 @@
 
             <p class="text-left">
                 <small>
-                    Selena attended Smith College and graduated with a degree in Economics and East Asian Studies with a focus on Chinese government, economics and language. Upon graduation she worked with the Wall Street Journal and began her Masters Degree in Education from the University of San Francisco, gaining a credential for middle school and high school students. Selena’s passion for education has led her to become involved in many neighborhood-based education initiatives in locally and abroad, including California, Massachusetts, Beijing, and Honduras. In her current work, she works with children from immigrant backgrounds to help their understanding of the educational system in which they currently reside. Much of this work is tutoring, but it also includes field trips to universities or going to different offices to see where and how people work. Selena speaks Mandarin and Spanish. Selena currently lives in San Mateo where she owns a greeting card business, where she lives with her husband and two dogs.
+                    <?php include 'pages/board_selena.php'; ?>
                 </small>
             </p>
         </div>
@@ -227,7 +204,7 @@
             </h2>
             <p class="text-left">
                 <small>
-                    Deanne founded the Meridian Health Foundation, dedicated to developing sustainable programs for children and youth, using education, the delivery of supportive health services, and the arts. Collaborative organizations include the Untied Nations Environment Programme, United Nations Department of Public Information, Full-Circle Learning, The Baum Foundation, TUNZA North America, and the City of San Francisco. She has organized and implemented humanitarian aid projects around the world, and most recently to Haiti. Her work as a public advocator has influenced solutions at a broader level. Interactive art and digital media exhibitions have brought attention to hot topics including: the “CO2 Cube” for the United Nations conference on Climate Change in Copenhagen, “Unlearning Intolerance” and “Paint for the Planet” both in the lobby of the United Nations Headquarters in New York City, “Melting Ice – A Hot Topic” in Monte Carlo for the Ministry of Culture, in Oslo at the Nobel Peace Center, and at Bozart Fine Arts Museum in Brussels. “Deserts, Drylands and Desertification” was held in Algiers, and another exhibit, “Trapped Inside” was installed at the United Nations Environment Programme headquarters in Nairobi. On-going educational programs for children and youth are associated with each theme. Current goals are focused on developing neighborhood community programs for Jr. youth in California as well as other work associated with the Bahá’i Faith.
+                    <?php include 'pages/board_deanne.php'; ?>
                 </small>
             </p>
         </div>
@@ -240,7 +217,7 @@
             </h2>
             <p class="text-left">
                 <small>
-                    Kevin, a Salvadorian native, came to the US when he was awarded an educational grant to attend California Institute of Technology, where he studied Computer Science and Economics. After graduation, he joined the Oracle Corporation where he specialized in web application development.  He currently is a software engineer at C3 Energy in Silicon Valley. Kevin’s main focus outside of work is the Junior Youth Spiritual Empowerment Program. After getting trained in “Releasing the Powers of Junior Youth” in August 2006, he has been working with junior youth groups and their animators all over Northern California. In December 2009, he moved to North Central San Mateo in order to dedicate a period of 10 years to the growth of the program in that neighborhood. His passion for working with this age group causes him to spend many hours a week with junior youth, looking for potential collaborators, talking to their parents about the program and engaging in the reading, art, service and recreational activities that are part of the programs. Kevin currently serves on the Regional Baha’i Council for the State of California, which is charged with supporting the Baha’i Community in the state and helping coordinate their community building endeavours.
+                    <?php include 'pages/board_kevin.php'; ?>
                 </small>
             </p>
         </div>
@@ -251,7 +228,7 @@
             </h2>
             <p class="text-left">
                 <small>
-                    Oliver, a Mauritius native, graduated in 2006 from the National University of Singapore, with a Bachelor in Computer Science. Coupled with an 18-month stint in Silicon Valley studying technological entrepreneurship at Stanford University, Oliver acquired the necessary skills to start his own company. In 2008, he co-founded Peekspy, a Singapore startup solving a problem millions of sports fans and concert-goers were experiencing around the world. Peekspy allowed the fans to see their view from their seat inside a venue before even purchasing their tickets. In March 2012, StubHub, the world’s largest online ticket marketplace owned by eBay, acquired Peekspy. Oliver is now a fulltime software engineer at StubHub. In the evenings, Oliver serves as a junior youth animator. He is interested in neighborhood community development, and works with groups of 11 to 15 year old youth for the material and spiritual progress of the community. Through the various service projects initiated by the groups of youth, they gradually transform the neighborhood into one where unity reigns and in turn develops their increasing capacities for serving humanity. Oliver is fluent in French, Mauritian Creole as well as English.
+                    <?php include 'pages/board_oliver.php'; ?>
                 </small>
             </p>
         </div>
---
+++ pages/donations.php
@@ -0,0 +1,24 @@
+<p>
+    100% of all contributions go directly to fund the programs we support.&nbsp; Foundation staff and business expenses are contributed voluntarily or by the for-profit sponsoring company. Meridian is a 501c3 not for profit charity, EIN # 68-0444965.&nbsp;&nbsp;
+</p>
+<p>
+    To make a donation by check, please send to:
+</p>
+<p>
+    Donate by Credit Card:
+</p>
+<p>
+    Click below&nbsp; $5.00 minimum
+</p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="image" alt="Make payments with PayPal - it's fast, free and secure!" name="I1" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif">
+    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHqQYJKoZIhvcNAQcEoIIHmjCCB5YCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBxP4pUfs3q8il0Ec+Fs9SFtCJTQBp0R8ANqhwczvPoJ+RVnsoKr0+pln0fbznrMIsni3pQaorT/J6cgj2sDiq9YHjyhynspzBzxxVZ2lm6EPTBAxSZ5IAw6hLrR4b22DdKXJebDq0utDQHbVwuMvsxezEYJhViG4eiWmOqGj987TELMAkGBSsOAwIaBQAwggElBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECGRuhQDAwAJcgIIBAHg9BvBmt8ELYLeu6gqsMPb3+NQOVftxZ9jQFDCyQzVoPQxiwDvcW0335bLYUVJukD1stcNTX/49SZewdCBAsvF7XPtrcNNZNm/ceouqnNQdoGQfdrgBrnMHY2+z0z9bcDQyyxMB8y7NV2M3Qs6htzx3wn7Hh1u/iYo/0rKma60gCB6Wc5i4AapjiFiauScmGbKs91EsK8Rutg6BQZexAxBfm8430ph+NGqLap75azgF6lcklgigZ8keMnUnErX+j4VgZ5UV3mKdGarJ7+CRypaujhnDwkZ3s8Jo253xkSi9/PJLZbmYUtmCjRJjoQSupRx4v0byCjrTSSFe03+4zkegggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0wNjAyMDYyMjE0MTNaMCMGCSqGSIb3DQEJBDEWBBQgb71m9PYd1aL2vmXyHVyDg6CQ4TANBgkqhkiG9w0BAQEFAASBgGW3VfbDG7eaT5EHq/Jg683zAADjGRF97BvJoICPrq5JeSfrtFyua3Hv6elpicfb7QF7S1LBHxIB/OmckHUVZCMmJ/+CEZuuTpyypyPJ3l6aQTxYFcvH3VwSX3IOLnPP6tsr46Aohqpyq+CNT8W/1YAKf0CqgL89nv0GLWLdcBRj-----END PKCS7----- ">
+    <br>
+    <input type="hidden" name="cmd" value="_s-xclick">
+</form>
+<p>
+    <strong>
+        Gift Option:
+    </strong>
+    To make this donation a holiday, birthday, memorial or other gift, please email us the name, address, occasion and your message.&nbsp; A card will be sent to the recipient to notify them that a charitable contribution has been made in their honor. Please email a description to the address below.
+</p>
