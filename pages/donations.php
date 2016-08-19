5470d1de158931ed27dfab517b4a92cf1cab8c3e
Kevin Trotter <kevintrotter@google.com>
2016-08-18 18:26:51 -0700

    changed donate button code for paypal

--- pages/donations.php
+++ pages/donations.php
@@ -4,18 +4,23 @@
 <p>
     To make a donation by check, you may send it to our <a href="#contact">PO Box listed below</a>.
 </p>
+<table class="donations">
+<tr>
+<td>
 <p>
     Donate by Credit Card:
 </p>
-<p>
-    Click below&nbsp; $5.00 minimum
-</p>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-    <input type="image" alt="Make payments with PayPal - it's fast, free and secure!" name="I1" src="https://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif">
-    <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHqQYJKoZIhvcNAQcEoIIHmjCCB5YCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBxP4pUfs3q8il0Ec+Fs9SFtCJTQBp0R8ANqhwczvPoJ+RVnsoKr0+pln0fbznrMIsni3pQaorT/J6cgj2sDiq9YHjyhynspzBzxxVZ2lm6EPTBAxSZ5IAw6hLrR4b22DdKXJebDq0utDQHbVwuMvsxezEYJhViG4eiWmOqGj987TELMAkGBSsOAwIaBQAwggElBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECGRuhQDAwAJcgIIBAHg9BvBmt8ELYLeu6gqsMPb3+NQOVftxZ9jQFDCyQzVoPQxiwDvcW0335bLYUVJukD1stcNTX/49SZewdCBAsvF7XPtrcNNZNm/ceouqnNQdoGQfdrgBrnMHY2+z0z9bcDQyyxMB8y7NV2M3Qs6htzx3wn7Hh1u/iYo/0rKma60gCB6Wc5i4AapjiFiauScmGbKs91EsK8Rutg6BQZexAxBfm8430ph+NGqLap75azgF6lcklgigZ8keMnUnErX+j4VgZ5UV3mKdGarJ7+CRypaujhnDwkZ3s8Jo253xkSi9/PJLZbmYUtmCjRJjoQSupRx4v0byCjrTSSFe03+4zkegggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0wNjAyMDYyMjE0MTNaMCMGCSqGSIb3DQEJBDEWBBQgb71m9PYd1aL2vmXyHVyDg6CQ4TANBgkqhkiG9w0BAQEFAASBgGW3VfbDG7eaT5EHq/Jg683zAADjGRF97BvJoICPrq5JeSfrtFyua3Hv6elpicfb7QF7S1LBHxIB/OmckHUVZCMmJ/+CEZuuTpyypyPJ3l6aQTxYFcvH3VwSX3IOLnPP6tsr46Aohqpyq+CNT8W/1YAKf0CqgL89nv0GLWLdcBRj-----END PKCS7----- ">
-    <br>
+</td>
+<td>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
     <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="95LY948T7WK4N">
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+</td>
+</tr>
+</table>
 <p>
     <strong>
         Gift Option:
